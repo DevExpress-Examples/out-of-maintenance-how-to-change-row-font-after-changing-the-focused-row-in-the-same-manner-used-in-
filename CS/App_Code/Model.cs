fd80f2b3bbc0f46ede6307a9cd78a4eaf798af3c
DevExpressExampleBot <scmaintenance@devexpress.com>
2023-09-25 19:19:13 +0400

    Change example status to out of maintenance

--- CS/App_Code/Model.cs
+++
@@ -1,6 +0,0 @@
-﻿public class Model {
-	public int Oid { get; set; }
-	public string Title { get; set; }
-	public string Message { get; set; }
-	public bool IsRead { get; set; }
-}
--- VB/App_Code/Model.vb
+++
@@ -1,6 +0,0 @@
-﻿Public Class Model
-    Public Property Oid() As Integer
-    Public Property Title() As String
-    Public Property Message() As String
-    Public Property IsRead() As Boolean
-End Class
